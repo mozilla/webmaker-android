fd5af17c62887daa930e67f66596fe25c1159517
Andrew Sliwinski <andrewsliwinski@acm.org>
2014-08-17 11:27:58 -0700

    Ignore npm debug files

--- views/activity/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    id: 'activity',
-    components: {
-        tabBar: require('../../components/tabBar'),
-        navigationBar: require('../../components/navigationBar')
-    },
-    template: require('./index.html'),
-    data: {}
-};
