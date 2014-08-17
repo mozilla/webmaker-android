548883ba8d94838fd4bc53040100c51f990e850f
Andrew Sliwinski <andrewsliwinski@acm.org>
2014-08-17 16:20:30 -0700

    Initial commit of data model sync layer

---
+++ test/unit/model.js
@@ -0,0 +1,39 @@
+window = {};
+
+var assert = require('assert');
+var model = require('../../lib/model');
+
+describe('Model', function () {
+    describe('interface', function () {
+        it('should expose expected objects', function () {
+            assert.equal(typeof model.history, 'object');
+            assert.equal(typeof model.user, 'object');
+            assert.equal(typeof model.apps, 'object');
+        });
+
+        it('should expose expected functions', function () {
+            assert.equal(typeof model.restore, 'function');
+            assert.equal(typeof model.save, 'function');
+            assert.equal(typeof model.observe, 'function');
+        });
+    });
+
+    describe('first run', function () {
+        it('should have the expected history', function () {
+            assert.deepEqual(model.history, {
+                ftu: true,
+                path: '/'
+            });
+        });
+
+        it('should have the expected user', function () {
+            // @todo
+            assert.deepEqual(model.user, {});
+        });
+
+        it('should have the expected apps', function () {
+            // @todo
+            assert.deepEqual(model.apps, []);
+        });
+    });
+});
