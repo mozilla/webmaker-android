dabff7c2b12339f3d04ee3571edb62203e603161
Andrew Sliwinski <andrewsliwinski@acm.org>
2014-08-17 16:19:44 -0700

    Add detail view stub

---
+++ views/detail/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    id: 'detail',
+    components: {
+        tabBar: require('../../components/tabBar'),
+        navigationBar: require('../../components/navigationBar')
+    },
+    template: require('./index.html'),
+    data: {}
+};
