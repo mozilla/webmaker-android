e659a23d8e95c354d742a47dba78fd2aebbb34ef
Kate Hudson <k88hudson@gmail.com>
2014-08-21 14:24:48 -0400

    Fix #63 - Hide footer on focus

--- components/tabBar/index.js
+++ components/tabBar/index.js
@@ -1,5 +1,17 @@
 module.exports = {
     id: 'tabBar',
     template: require('./index.html'),
-    data: {}
+    data: {},
+    attached: function () {
+        var el = this.$el;
+        var inputs = document.querySelectorAll('input, te');
+        function onFocus() {
+            el.style.opacity = 0;
+        }
+        function onBlur() {
+            el.style.opacity = '';
+        }
+        document.addEventListener('focus', onFocus, true);
+        document.addEventListener('blur', onBlur, true);
+    }
 };
