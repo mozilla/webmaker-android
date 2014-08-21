acd02245036ec2882791ac903fdb2745a277db9f
Kate Hudson <k88hudson@gmail.com>
2014-08-21 17:31:20 -0400

    Fix #39 - First run stuff

---
+++ views/ftu-3/index.js
@@ -0,0 +1,6 @@
+var view = require('../../lib/view');
+
+module.exports = view.extend({
+    id: 'ftu-3',
+    template: require('./index.html')
+});
--- views/ftu/index.js
+++ views/ftu/index.js
@@ -1,6 +1,28 @@
 var view = require('../../lib/view');
+var i18n = require('../../lib/i18n');
 
 module.exports = view.extend({
     id: 'ftu',
-    template: require('./index.html')
+    template: require('./index.html'),
+    directives: {
+        cycle: function (raw) {
+            var self = this;
+            var localized = raw.map(function (key) {
+                return i18n.get(key);
+            });
+            var i = 0;
+            function changeWord() {
+                if (i >= localized.length) {
+                    i = 0;
+                }
+                self.el.innerHTML = localized[i];
+                i++
+            }
+            setInterval(changeWord, 3000);
+            changeWord();
+        }
+    },
+    data: {
+      personas: ['vendors', 'students', 'teachers', 'parents', 'doctors', 'activists', 'journalists', 'scientists', 'you']
+    }
 });
