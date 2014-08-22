df5683dbd5c875bf703889e97625032b4a2983dc
Kate Hudson <k88hudson@gmail.com>
2014-08-22 12:44:08 -0400

    lint updates, destroy interval

--- views/ftu/index.js
+++ views/ftu/index.js
@@ -5,24 +5,40 @@ module.exports = view.extend({
     id: 'ftu',
     template: require('./index.html'),
     directives: {
-        cycle: function (raw) {
-            var self = this;
-            var localized = raw.map(function (key) {
-                return i18n.get(key);
-            });
-            var i = 0;
-            function changeWord() {
-                if (i >= localized.length) {
-                    i = 0;
+        cycle: {
+            update: function (raw) {
+                var self = this;
+                var localized = raw.map(function (key) {
+                    return i18n.get(key);
+                });
+                var i = 0;
+                function changeWord() {
+                    if (i >= localized.length) {
+                        i = 0;
+                    }
+                    self.el.innerHTML = localized[i];
+                    i++;
                 }
-                self.el.innerHTML = localized[i];
-                i++
+                clearInterval(this.interval);
+                this.interval = setInterval(changeWord, 3000);
+                changeWord();
+            },
+            unbind: function () {
+                clearInterval(this.interval);
             }
-            setInterval(changeWord, 3000);
-            changeWord();
         }
     },
     data: {
-      personas: ['vendors', 'students', 'teachers', 'parents', 'doctors', 'activists', 'journalists', 'scientists', 'you']
+        personas: [
+            'vendors',
+            'students',
+            'teachers',
+            'parents',
+            'doctors',
+            'activists',
+            'journalists',
+            'scientists',
+            'you'
+        ]
     }
 });
