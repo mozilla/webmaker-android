31534698d00f17fdb05bd9fc8886797f71210742
Andrew Sliwinski <andrewsliwinski@acm.org>
2014-09-03 17:00:50 -0700

    Fix issues with 'gulp clean' on Windows. Drop use of gulp-shell

--- gulp/clean.js
+++ gulp/clean.js
@@ -1,11 +1,19 @@
+var fs = require('fs-extra');
+var glob = require('glob');
 var gulp = require('gulp');
-var shell = require('gulp-shell');
 
-module.exports = shell.task([
-    'rm -rf ./build',
-    'mkdir -p build',
-    'cp -a ./static/. ./build/',
-    'cat `find ./views -name "*.less"` > ./build/styles/views.less',
-    'cat `find ./components -name "*.less"` > ./build/styles/components.less',
-    'cat `find ./blocks -name "*.less"` > ./build/styles/blocks.less'
-]);
+module.exports = function (callback) {
+    fs.removeSync('./build');
+    fs.mkdirsSync('build');
+    fs.copySync('./static/.', './build/');
+
+    glob('./{views,components,blocks}/**/*.less', function (err, files) {
+        if (err) return callback(err);
+
+        var file = '';
+        for (var i in files) {
+            file += fs.readFileSync(files[i]) + '\n';
+        }
+        fs.writeFile('./build/styles/bundle.less', file, callback);
+    });
+};
