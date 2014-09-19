d4bca316815f2a65800369d637b3e56bf95cc663
Andrew Sliwinski <andrewsliwinski@acm.org>
2014-09-18 22:27:21 -0700

    Add 'discover' stub

---
+++ views/discover/index.js
@@ -0,0 +1,12 @@
+var view = require('../../lib/view');
+
+module.exports = view.extend({
+    id: 'discover',
+    template: require('./index.html'),
+    data: {
+        title: 'Discover'
+    },
+    created: function () {
+        this.$data.created = this.model.apps;
+    }
+});
