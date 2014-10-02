0107343f83d67e3fb578cc5c75cc611aa5cea020
Tim <tim.kolberg@intradesys.com>
2014-10-02 11:35:37 +0200

    [#161] added data dump to view
    added view to show raw application data

--- views/data/index.js
+++ views/data/index.js
@@ -5,6 +5,7 @@ var view = require('../../lib/view');
 module.exports = view.extend({
     id: 'data',
     template: require('./index.html'),
+	collectedData: {},
     created: function () {
         var self = this;
 
@@ -15,5 +16,13 @@ module.exports = view.extend({
         // Bind app
         self.$data.app = app.data;
         self.title = app.data.name;
+
+		// Fetch collected Data
+		self.collectedData = {
+			0: {'name': 'fgsdfgsdfg', 'age': '12'},
+			1: {'name': 'dsfgd', 'age': '13'},
+			2: {'name': 'fgsdfsfdgdsf gsdfg', 'age': '15'},
+			3: {'name': 'cxvbcvbx', 'age': '35'}
+		};
     }
 });
