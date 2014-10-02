da5f5bd49e440135bd531fdfe11a3e09d75eebb4
Kate Hudson <k88hudson@gmail.com>
2014-10-02 15:36:00 -0400

    fixup syntax

--- gulp/less.js
+++ gulp/less.js
@@ -3,7 +3,7 @@ var less = require('gulp-less');
 
 module.exports = function() {
     var src = gulp.src('./build/styles/common.less');
-    var dest = gulp.dest('./build/styles/');
+    var dest = gulp.dest('./build/styles');
 
     src.pipe(less()).pipe(dest).pipe(gulp.dest('./build/assets/publish'))
 };
