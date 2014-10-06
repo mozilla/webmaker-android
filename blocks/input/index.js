f917481470963fb7d39731fd031095023b5fd96c
Timm Haucke <timm.haucke@intradesys.com>
2014-10-06 17:40:03 +0200

    [#158]add block editor for choice between single and multi line text in the inpu t brick and give the input brick the ability to choose between these two

--- blocks/input/index.js
+++ blocks/input/index.js
@@ -8,29 +8,37 @@ module.exports = {
 		test: 'my test value',
 
         attributes: {
-			value: {
-				label: 'Value',
-				type: 'string',
-				value: 'Default Value',
-				skipAutoRender: true
-			},
-			label: {
-				label: 'Label',
-				type: 'string',
-				value: 'I am your Label',
-				skipAutoRender: true
-			}
+            inputType: {
+                label: 'Input Type',
+                type: 'dropdownChoice',
+                options: ['Single Line Text', 'Long Text'],
+                value: 0,
+                skipAutoRender: true
+            },
+            label: {
+                label: 'Title',
+                type: 'string',
+                value: '',
+                placeholder: 'Your title goes here',
+                skipAutoRender: true
+            },
+			color: {
+               label: 'Title Text Color',
+               type: 'color',
+               value: '#638093'
+           }
         }
     },
 	ready: function() {
 		var self = this;
 
 		if(self.$parent.$parent.$data.params.mode !== 'play') {
-			self.$el.querySelector('input').disabled = 'disabled';
+            if (self.$el.querySelector('input')) self.$el.querySelector('input').disabled = 'disabled';
+            if (self.$el.querySelector('textfield')) self.$el.querySelector('textfield').disabled = 'disabled';
 		}
 
 		this.$watch('attributes.value.value', function (value, mutation) {
 			console.log('input value changed');
-		})
+		});
 	}
 };
