290e2bb2c8d3ba83429fff6af64719428e262818
Timm Haucke <timm.haucke@intradesys.com>
2014-10-09 18:57:37 +0200

    [#160] add basic data block

---
+++ blocks/data/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    className: 'data',
+    template: require('./index.html'),
+    data: {
+        name: 'Data',
+        icon: '/images/blocks_text.png',
+        attributes: {
+            
+        }
+    }
+};
