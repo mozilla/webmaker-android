9950ef96d6598ca070085165920032f65111e855
Jan David Wasem <jandavid.wasem@intradesys.com>
2014-10-10 17:42:52 +0200

    Added block for data view

--- blocks/data/index.js
+++ blocks/data/index.js
@@ -1,3 +1,5 @@
+var Data = require('../../lib/data');
+
 module.exports = {
     className: 'data',
     template: require('./index.html'),
@@ -6,6 +8,15 @@ module.exports = {
         icon: '/images/blocks_text.png',
         attributes: {
             
-        }
+        },
+        currentDataSets: {},
+    },
+    ready: function (){
+        var self = this;
+
+		// Fetch collected Data
+		var data = new Data('foooobar');
+		self.currentDataSets = data.getAllDataSets();
+		console.log(self.currentDataSets);
     }
 };
