772534b4c84433af2b4956609e9543fef07bdde4
Timm Haucke <timm.haucke@intradesys.com>
2014-10-10 17:44:39 +0200

    [#161] add select all checkbox functionality and a delete button

--- components/dataRepresentation/index.js
+++ components/dataRepresentation/index.js
@@ -2,6 +2,43 @@ module.exports = {
     id: 'dataRepresentation',
     template: require('./index.html'),
     data: {
-
+        allRead: false
+    },
+    methods: {
+        formatUnixTime: function(unix) {
+            var date = new Date(unix);
+            return date.toString();
+        },
+        notifyToggleRead: function() {
+            var allRead = this.allRead;
+            var dataSet = this.$data.dataSet;
+            var allDataRead = true;
+            for (i = 0; i < dataSet.length; i++) {
+                var data = dataSet[i];
+                if (data && !data.isRead) allRead = false;
+            }
+            console.log(allRead);
+            allRead = allDataRead;
+        },
+        toggleReadAll: function() {
+            this.allRead = !this.allRead;
+            var allRead = this.allRead;
+            var dataSet = this.$data.dataSet;
+            for (i = 0; i < dataSet.length; i++) {
+                var data = dataSet[i];
+                if (data) data.isRead = allRead;
+            }
+        },
+        removeSelected: function()
+        {
+            var dataSet = this.$data.dataSet;
+            for (i = 0; i < dataSet.length; i++) {
+                var data = dataSet[i];
+                if (data && data.isRead) {
+                    console.log(data);
+                    data.$delete();
+                }
+            }
+        }
     }
 };
