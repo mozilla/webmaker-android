f8926f6ca4c3dae1fb6e614151dec1806027d316
Timm Haucke <timm.haucke@intradesys.com>
2014-10-14 18:46:02 +0200

    [#160,161]add label and data attribute to the data block which makes it possible to see and sort the data

--- blocks/data/index.js
+++ blocks/data/index.js
@@ -7,17 +7,27 @@ module.exports = {
         name: 'Data',
         icon: '/images/blocks_text.png',
         attributes: {
-            
+            label: {
+                label: 'Header Text',
+                type: 'string',
+                value: '',
+                placeholder: 'Responses',
+                skipAutoRender: true
+            },
+            data: {
+                label: 'Sorting',
+                type: 'data',
+                value: false,
+                skipAutoRender: true
+            }
         },
         currentDataSets: {},
         isInteractive: false
     },
     ready: function (){
         var self = this;
-
 		// Fetch collected Data
 		var data = new Data('foooobar');
 		self.currentDataSets = data.getAllDataSets();
-		console.log(self.currentDataSets);
     }
 };
