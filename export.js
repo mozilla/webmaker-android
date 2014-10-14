17ac60419a2523f41efa15d3cd293a9cf65911fd
Kate Hudson <k88hudson@gmail.com>
2014-10-14 13:01:18 -0400

    added copySharedAssets too

--- export.js
+++ export.js
@@ -2,9 +2,33 @@
 var gulp = require('gulp');
 var path = require('path');
 
+var buildDir = __dirname + '/build';
+
+function handleError(stream, cb) {
+    stream.on('end', function() {
+        cb(null);
+    });
+    stream.on('error', function (err) {
+        cb(err);
+    });
+}
+
 module.exports = {
-    copyPublishAssets: function (dest) {
-        gulp.src(path.join(__dirname, './build/publish-assets/**/*'))
-            .pipe(gulp.dest(dest));
+    copySharedAssets: function (dest, cb) {
+        var s = gulp.src([
+            buildDir + '/content',
+            buildDir + '/fonts',
+            buildDir + '/images',
+            buildDir + '/styles'
+        ]).pipe(gulp.dest(dest));
+        handleError(s, cb);
+    },
+    copyPublishAssets: function (dest, cb) {
+        var s = gulp.src([
+            buildDir + '/publish-assets/**/*',
+            // Exclude sample app.js
+            '!' + buildDir + '/publish-assets/app.js'
+        ]).pipe(gulp.dest(dest));
+        handleError(s, cb);
     }
 };
