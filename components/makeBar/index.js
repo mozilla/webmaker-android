d41a1a974a6eb6a4fc49ed17278a9cb618bd09ec
jmarlena <jmarlena.canepa@gmail.com>
2014-10-15 15:43:06 -0700

    added comma

--- components/makeBar/index.js
+++ components/makeBar/index.js
@@ -11,7 +11,7 @@ module.exports = {
             } else {
                 this.$el.className = '';
             }
-        }
+        },
         goBack: function (e) {
             e.preventDefault();
             global.history.back();
