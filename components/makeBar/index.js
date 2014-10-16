d6dbd76c7b97c9e334baadabfba59576ff132ee0
Luke Pacholski <luke.pacholski@gmail.com>
2014-10-16 10:49:04 -0700

    Added toggle button to makeBar

--- components/makeBar/index.js
+++ components/makeBar/index.js
@@ -1,20 +1,4 @@
 module.exports = {
     id: 'makeBar',
     template: require('./index.html'),
-    methods: {
-        onClick: function (e) {
-            var href = e.target.href;
-            var section = href.split('/').pop();
-
-            if (section === 'edit') {
-                this.$el.className = 'edit';
-            } else {
-                this.$el.className = '';
-            }
-        },
-        goBack: function (e) {
-            e.preventDefault();
-            global.history.back();
-        }
-    }
 };
