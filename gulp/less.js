cefebc73935fecd1ef85ce0c449d37850d404f57
Habib <habibullah.zahoori@gmail.com>
2014-10-19 01:35:00 -0400

    Isue #312

--- gulp/less.js
+++ gulp/less.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var plumber = require('gulp-plumber');
 
 module.exports = function() {
     var src = gulp.src('./build/styles/common.less');
     var dest = gulp.dest('./build/styles');
-
-    src.pipe(less()).pipe(dest).pipe(gulp.dest('./build/publish-assets'));
+       src
+    .pipe(plumber())
+    .pipe(less()).pipe(dest).pipe(gulp.dest('./build/publish-assets'));
 };
