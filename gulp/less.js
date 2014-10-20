d06ea2c54d263d6a8518177fc730e7cac411567c
Habib <habibullah.zahoori@gmail.com>
2014-10-20 19:49:48 -0400

    Package.Json and Less.js repushing

--- gulp/less.js
+++ gulp/less.js
@@ -5,12 +5,7 @@ var plumber = require('gulp-plumber');
 module.exports = function() {
     var src = gulp.src('./build/styles/common.less');
     var dest = gulp.dest('./build/styles');
-<<<<<<< HEAD
        src
     .pipe(plumber())
     .pipe(less()).pipe(dest).pipe(gulp.dest('./build/publish-assets'));
-=======
-
-    src.pipe(less()).pipe(dest);
->>>>>>> eebbe46676390573b267bc2c985cacda25905711
 };
