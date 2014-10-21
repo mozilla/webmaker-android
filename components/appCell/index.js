bc8137b928d30d6343d0f11e2679db3aec6c3615
Kate Hudson <k88hudson@gmail.com>
2014-10-21 19:24:44 -0400

    [#391] Added some missing stuff to locale/en_US

--- components/appCell/index.js
+++ components/appCell/index.js
@@ -1,5 +1,11 @@
+var i18n = require('../../lib/i18n');
+
 module.exports = {
     className: 'app-cell',
     template: require('./index.html'),
-    data: {}
+    computed: {
+        guestKey: function() {
+            return i18n.get('Guest');
+        }
+    }
 };
