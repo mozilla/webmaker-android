a108e0790c4021a12c7aca69e34650b4f6109d0f
Kate Hudson <k88hudson@gmail.com>
2014-11-06 17:04:11 -0500

    actually return streams

--- gulp/jscs.js
+++ gulp/jscs.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var jscs = require('gulp-jscs');
 
 module.exports = function () {
-    gulp.src('./{lib,blocks,components,views}/**/*.js')
+    return gulp.src('./{lib,blocks,components,views}/**/*.js')
         .pipe(jscs());
 };
--- gulp/less.js
+++ gulp/less.js
@@ -6,7 +6,11 @@ var sourcemaps = require('gulp-sourcemaps');
 module.exports = function() {
     var src = gulp.src('./build/styles/common.less');
     var dest = gulp.dest('./build/styles');
-       src
-    .pipe(plumber())
-    .pipe(sourcemaps.init()).pipe(less()).pipe(sourcemaps.write()).pipe(dest).pipe(gulp.dest('./build/publish-assets'));
+    return src
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(sourcemaps.write())
+        .pipe(dest)
+        .pipe(gulp.dest('./build/publish-assets'));
 };
