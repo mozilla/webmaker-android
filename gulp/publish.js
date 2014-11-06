543a74f50de552b82a7051262a65ec1cdc4255e2
Kate Hudson <k88hudson@gmail.com>
2014-11-06 17:01:11 -0500

    [Fix #464 ] Fix gulp dev crashing

--- gulp/publish.js
+++ gulp/publish.js
@@ -2,23 +2,25 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var fs = require('fs');
 var file = require('gulp-file');
+var merge = require('merge-stream');
 
 var templates = require('../lib/templates.json');
 var json = templates[2];
 
 module.exports = function () {
     var dest = './build/publish-assets';
-    gulp.src('./publish/index.js')
+    var js = gulp.src('./publish/index.js')
         .pipe(browserify({
             insertGlobals: false,
             transform: ['partialify', 'bulkify']
         })).pipe(gulp.dest(dest));
 
-    gulp.src('./publish/index.html')
+    var html = gulp.src('./publish/index.html')
         .pipe(gulp.dest(dest));
 
     // Just for example
     var string = 'window.App=' + JSON.stringify(json) + ';';
-    file('app.js', string).pipe(gulp.dest(dest));
+    var app = file('app.js', string).pipe(gulp.dest(dest));
 
+    return merge(js, html, app);
 };
