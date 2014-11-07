a3bc919f6096deab5c883134fa4dbc336715f24e
Andrew Sliwinski <andrewsliwinski@acm.org>
2014-11-06 19:37:10 -0800

    Resolve issue with Firebase module blocking mocha exit

--- gulp/unit.js
+++ gulp/unit.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
+var exit = require('gulp-exit');
 var mocha = require('gulp-mocha');
 
 module.exports = function () {
     var src = gulp.src('./test/unit/*.js');
     src.pipe(mocha({
         reporter: 'spec'
-    }));
+    })).pipe(exit());
 };
