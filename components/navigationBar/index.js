a5810c0b97ff5b52a8840269e2f837b54c6a5ba0
Luke Pacholski <luke.pacholski@gmail.com>
2014-11-12 20:41:34 +0100

    Back button fixes for editor

--- components/navigationBar/index.js
+++ components/navigationBar/index.js
@@ -1,8 +1,21 @@
+var page = require('page');
+
 module.exports = {
     id: 'navigationBar',
     template: require('./index.html'),
     data: {
         goBack: function (e) {
+            if (this.$data.app) {
+                var enteredFrom = this.$data.app.enteredEditorFrom || '';
+                if (enteredFrom === 'templates') {
+                    page('/templates');
+                    return;
+                }
+                if (enteredFrom === 'profile') {
+                    page('/profile');
+                    return;
+                }
+            }
             e.preventDefault();
             global.history.back();
         }
--- views/edit/index.js
+++ views/edit/index.js
@@ -5,7 +5,7 @@ module.exports = view.extend({
     id: 'edit',
     template: require('./index.html'),
     data: {
-        back: '/profile',
+        back: true,
         doneLabel: 'Publish'
     },
     created: function () {
