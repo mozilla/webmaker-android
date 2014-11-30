fc5e02a6a31eff8b1e75aab2fe8e9b9e9e7b27a2
Kate Hudson <k88hudson@gmail.com>
2014-11-30 17:41:35 -0500

    autoprefixer

--- gulp/less.js
+++ gulp/less.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 var handleErrors = require('./error');
 
 module.exports = function() {
@@ -10,8 +11,13 @@ module.exports = function() {
     var stream = src
         .pipe(handleErrors())
         .pipe(sourcemaps.init())
-	.pipe(less())
+        .pipe(less())
         .pipe(minifyCSS({keepBreaks:false}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false,
+            remove: true
+        }))
         .pipe(sourcemaps.write())
         .pipe(dest)
         .pipe(gulp.dest('./build/publish-assets'));
