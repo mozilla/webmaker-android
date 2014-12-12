9137cb959471ac5cd41c187e4309768a3146c890
Luke Pacholski <luke.pacholski@gmail.com>
2014-12-12 11:32:59 -0800

    Colorpicker updates

--- views/color-picker/index.js
+++ views/color-picker/index.js
@@ -3,10 +3,15 @@ var utils = require('../../lib/utils');
 
 var colorGroups = [
     '#333444',
-    '#FC5D5E',
-    '#FEE444',
-    '#1CB0B4',
-    '#31ABDF'
+    '#1e79da',
+    '#1F9CDF',
+    '#18dea6',
+    '#39d91f',
+    '#e7ce17',
+    '#ea6517',
+    '#e71a18',
+    '#ec1a6e',
+    '#d91fd6'
 ];
 
 var ref;
@@ -31,6 +36,7 @@ module.exports = view.extend({
             self.$root.isReady = true;
             if (!val) return;
             $data.selectedColor = val;
+
             $data.colors.forEach(function (arr, i) {
                 arr.forEach(function (color) {
                     if (color === val) {
@@ -39,24 +45,37 @@ module.exports = view.extend({
                 });
                 $data.selectedGroup = $data.selectedGroup || 0;
             });
-        });
 
+            if (colorGroups.indexOf($data.selectedColor) > -1) {
+                $data.mode = 'group';
+            } else {
+                $data.mode = 'single';
+            }
+        });
     },
     data: {
         title: 'Select Color',
+        selectFromGroup: function (color) {
+            this.$data.selectedColor = color;
+            ref.set(color);
+        },
         onSelect: function (color) {
             this.$data.selectedColor = color;
+            this.$data.mode = 'single';
             ref.set(color);
         },
         onGroupSelect: function (i) {
             this.$data.selectedGroup = i;
-            this.onSelect(colorGroups[i]);
+            this.selectFromGroup(colorGroups[i]);
+            this.$data.mode = 'group';
         },
         colorGroups: colorGroups,
         colors: colorGroups.map(function (base) {
             var tints = [];
             for (var i = -5; i < 10; i++) {
-                tints.push(utils.shadeColor(base, i * 6));
+                var colorString = utils.shadeColor(base, i * 6);
+                colorString = colorString.toUpperCase();
+                tints.push(colorString);
             }
             return tints;
         })
