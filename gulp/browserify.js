ee93432a1d142973df8ec5b80f7f7267b1ec5808
Kate Hudson <k88hudson@gmail.com>
2014-12-19 16:03:21 -0500

    ok done

--- gulp/browserify.js
+++ gulp/browserify.js
@@ -1,12 +1,11 @@
-var gulp = require('gulp');
 var run = require('gulp-run');
-var browserify = require('browserify');
-var buffer = require('vinyl-buffer');
-var source = require('vinyl-source-stream');
-var uglify = require('gulp-uglify');
-var handleErrors = require('./error');
-var sourcemaps = require('gulp-sourcemaps');
+ 
+function browserify(src, dest) {
+    return 'browserify ' + src + ' -d -t partialify -t bulkify ' +
+        ' | exorcist ' + dest + '.map > ' + dest;
+}
+
 
 module.exports = function (done) {
-    run('browserify ./lib/index.js -d -t [partialify bulkify] -o ./build/index.js').exec(done);
+    run(browserify('./lib/index.js', './build/index.js')).exec(done);
 };
