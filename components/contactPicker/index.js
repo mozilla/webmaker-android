7ec42eada94397f2b58d1593c9d06280632d2495
Gvn Lazar Suntop <gavin@gsuntop.com>
2015-01-07 12:18:31 -0800

    basic display of dummy data in alpha groups

--- components/contactPicker/index.js
+++ components/contactPicker/index.js
@@ -5,6 +5,8 @@ module.exports = {
         this.$on('openContactPicker', function (event) {
             this.show = true;
         });
+
+        this.modelData();
     },
     methods: {
         onSave: function (e) {
@@ -13,7 +15,59 @@ module.exports = {
         onCancel: function (e) {
             this.selectedColor = this.originalColor;
             this.show = false;
+        },
+        modelData: function () {
+            var modeled = {};
+
+            // Sort contacts into alphabetical order
+            this.contacts = this.contacts.sort(function (a, b) {
+                return a.name > b.name;
+            });
+
+            // Create an object with alpha-keys to group by first name
+            this.contacts.forEach(function (contact) {
+                var firstLetter = contact.name[0].toUpperCase();
+
+                if (!modeled[firstLetter]) {
+                    modeled[firstLetter] = [];
+                }
+
+                modeled[firstLetter].push(contact);
+            });
+
+            this.modeledContacts = modeled;
         }
     },
-    data: {}
+    data: {
+        contacts: [
+            {
+                name: 'Bob Bobbington',
+                number: 1235433333
+            },
+            {
+                name: 'Eoo Eoobington',
+                number: 1235433333
+            },
+            {
+                name: 'M Mbington',
+                number: 1235433333
+            },
+            {
+                name: 'Dude Bobbington',
+                number: 1235433333
+            },
+            {
+                name: 'earl Earlbington',
+                number: 1235433333
+            },
+            {
+                name: 'Joe Joebington',
+                number: 1235433333
+            },
+            {
+                name: 'Frank Frankbington',
+                number: 1235433333
+            }
+        ]
+    }
 };
