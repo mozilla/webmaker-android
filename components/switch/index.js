7e68c2beb67205e72b653b4dc1db34d1ae343efa
Gvn Lazar Suntop <gavin@gsuntop.com>
2015-01-12 17:18:34 -0800

    pulling newest & featured from firebase

--- components/switch/index.js
+++ components/switch/index.js
@@ -1,13 +1,20 @@
 module.exports = {
     id: 'switch',
     template: require('./index.html'),
+    ready: function () {
+        this.$watch('value', function (newVal) {
+            var chosenOption = 0 + newVal; // convert Boolean to Number
+            chosenOption = this.options[chosenOption];
+            this.$dispatch('switchValueChanged', chosenOption);
+        });
+    },
     data: {
         value: false,
         options: ['Off', 'On']
     },
     methods: {
-      onClick: function (event) {
-        this.value = !this.value;
-      }
+        onClick: function (event) {
+            this.value = !this.value;
+        }
     }
 };
