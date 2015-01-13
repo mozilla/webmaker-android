cb6e5602e636307b440428a7df9cc8b9b52a51a0
Kate Hudson <k88hudson@gmail.com>
2015-01-13 13:02:03 -0500

    update cordova.js fake

--- static/cordova.js
+++ static/cordova.js
@@ -1,1 +1,10 @@
-navigator.whatever = true;
+// This is a stand-in for the real cordova code, so that this code-base can
+// still run in a web browser;
+
+// todo: shim as needed
+// https://github.com/apache/cordova-plugin-contacts/blob/master/doc/index.md
+navigator.contacts = {
+    create: function () {},
+    find: function () {},
+    pickContact: function () {}
+};
