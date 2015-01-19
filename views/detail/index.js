61c98b995ab862e6356e0b32776139f3e1aa04c0
Kate Hudson <k88hudson@gmail.com>
2015-01-19 16:39:10 -0500

    a few more touch ups

--- views/detail/index.js
+++ views/detail/index.js
@@ -9,55 +9,21 @@ module.exports = view.extend({
         back: true,
         title: 'App'
     },
-    methods: {
-        create: function () {
-            var self = this;
-            var options = {};
-            if (self.$data.isTemplate) {
-                options.template = self.$root.params.id;
-            } else {
-                options.data = self.$data.app;
-            }
-            var app = self.$root.storage.createApp(options);
-
-            self.$data.create = false;
-            self.$root.$data.enteredEditorFrom = '/templates';
-            self.$root.isReady = false;
-            setTimeout(function () {
-                self.$root.isReady = true;
-                self.page('/make/' + app.id);
-            }, 100);
-
-        }
-    },
     created: function () {
         var self = this;
         var id = self.$root.params.id;
 
         self.$data.id = id;
-        self.$data.isTemplate = self.$root.params.template;
+  
         // Fetch app
-        if (self.$data.isTemplate) {
-            templates.forEach(function (template) {
-                var data;
-                if (template.id === id) {
-                    data = clone(template);
-                    data.id = id;
-                    self.$data.app = data;
-                }
-                self.$root.isReady = true;
-            });
-        } else {
-            var app = self.$root.storage.getApp(id);
-            if (app.data) {
-                self.$root.isReady = true;
-                self.$data.app = app.data;
-            }
-            self.$on(id, function (val) {
-                self.$root.isReady = true;
-                self.$data.app = val;
-            });
+        var app = self.$root.storage.getApp(id);
+        if (app.data) {
+            self.$root.isReady = true;
+            self.$data.app = app.data;
         }
-
+        self.$on(id, function (val) {
+            self.$root.isReady = true;
+            self.$data.app = val;
+        });
     }
 });
