c237121ef5f4a016799074acda0a04845ba90255
Kate Hudson <k88hudson@gmail.com>
2015-01-21 17:08:27 -0500

    Fix contact picker and toggle. [#725, #742]

--- components/toggle/index.js
+++ components/toggle/index.js
@@ -2,6 +2,14 @@ module.exports = {
     className: 'toggle',
     template: require('./index.html'),
     paramAttributes: ['disabled', 'checked'],
+    methods: {
+        onClick: function (e) {
+            e.preventDefault();
+            if (this.disabled) return;
+            this.checked = !this.checked;
+            console.log(this.checked);
+        }
+    },
     ready: function () {
         // parse non booleans
         // eg: values passed in as a "disabled" param instead of v-with
