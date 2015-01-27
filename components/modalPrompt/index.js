3d3861a3091488bf54628a1c6203494a96535502
Gavin Lazar Suntop <gavin@gsuntop.com>
2015-01-26 17:00:31 -0800

    building and styling modal prompt

---
+++ components/modalPrompt/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+    id: 'modal-prompt',
+    ready: function () {
+        var self = this;
+
+        self.$on('openModalPrompt', function (event) {
+            self.open();
+        });
+
+        self.$on('closeModalPrompt', function (event) {
+            self.close();
+        });
+    },
+    methods: {
+        open: function () {
+            this.isOpen = true;
+            this.$el.classList.add('active');
+
+        },
+        close: function (event) {
+            this.isOpen = false;
+            this.$el.classList.remove('active');
+        }
+    },
+    data: {
+        isOpen: false
+    },
+    template: require('./index.html')
+};
