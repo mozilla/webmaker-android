ff6614c1dae71e0f023b7e1866ca084ebb6e279d
Pomax <pomax@nihongoresources.com>
2015-02-10 09:48:34 -0800

    removed the .attached handling for tabBar

--- components/tabBar/index.js
+++ components/tabBar/index.js
@@ -1,25 +1,5 @@
 module.exports = {
     id: 'tabBar',
     template: require('./index.html'),
-    data: {},
-    attached: function () {
-        var el = this.$el;
-        function onFocus(e) {
-            if (!e.target.tagName) return;
-            var tagName = e.target.tagName.toLowerCase();
-            var type = e.target.getAttribute('type');
-            if (['input', 'textarea'].indexOf(tagName) <= -1) {
-                return;
-            }
-            if (type === 'checkbox') {
-                return;
-            }
-            el.style.display = 'none';
-        }
-        function onBlur() {
-            el.style.display = 'block';
-        }
-        document.addEventListener('focus', onFocus, true);
-        document.addEventListener('blur', onBlur, true);
-    }
+    data: {}
 };
