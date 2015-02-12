c0b4497cb32a94078ae12e2a565c0ccfa9d8a642
Kate Hudson <k88hudson@gmail.com>
2015-02-11 18:51:46 -0800

    fix random loops that happen when share is clicked from sidebarmenu

--- views/make/index.js
+++ views/make/index.js
@@ -100,13 +100,6 @@ module.exports = view.extend({
             self.$dispatch('openModalPrompt');
         });
 
-        self.$on('sideMenuShareClick', function (event) {
-            this.page(
-                '/make/' +
-                self.$root.$data.params.id +
-                '/share?publish=true');
-        });
-
         self.$on('sideMenuDataClick', function (event) {
             this.mode = 'data';
         });
