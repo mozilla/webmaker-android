25fb2f8f45e0ebcd8783bc6ee1cc08a995b182bb
Ali Al Dallal <ali@alicoding.com>
2015-02-18 11:18:26 -0500

    Fix #1141 - Add locale selector to developer menu

--- views/healthcheck/index.js
+++ views/healthcheck/index.js
@@ -3,6 +3,8 @@ var clone = require('clone');
 var _config = require('../../config');
 var config = clone(_config);
 var packageJSON = clone(config.package);
+var i18n = require('../../lib/i18n.js');
+var langmap = require('langmap');
 delete config.package;
 
 module.exports = view.extend({
@@ -13,5 +15,22 @@ module.exports = view.extend({
         back: '/profile',
         config: config,
         package: packageJSON
+    },
+    computed: {
+      options: function () {
+          return i18n.supportedLangs();
+      }
+    },
+    methods: {
+      langmap: function (locale) {
+          return langmap[locale].nativeName;
+      },
+      selectedLang: function (e) {
+          i18n.setLocale(e.target.value, true);
+          this.page(this.$data.back);
+      }
+    },
+    ready: function () {
+        this.$data.currentLang = this.model.data.session.locale;
     }
 });
