8903779084b7319f9d4b6cfa2cb4efb172ebab09
Kate Hudson <k88hudson@gmail.com>
2015-02-20 19:28:03 -0500

    [#1274] Remove old locale files

--- gulp/download-locales.js
+++ gulp/download-locales.js
@@ -1,13 +1,23 @@
 var downloadLocales = require('webmaker-download-locales');
+var glob = require('glob');
+var fs = require('fs-extra');
 
 // Which languages should we pull down?
 // Don't include en-US because that's committed into git already
 var supportedLanguages = ['id', 'en_CA', 'es_CL', 'fr', 'nl', 'es_MX', 'cs', 'sv', 'bn_BD'];
 
 module.exports = function (callback) {
-    downloadLocales({
-        app: 'webmaker-app',
-        dir: 'locale',
-        languages: supportedLanguages
-    }, callback);
+
+    glob('!./locale/!(en_US)/**.json', function (err, files) {
+        files.forEach(function (file) {
+            fs.removeSync(file);
+        });
+        downloadLocales({
+            app: 'webmaker-app',
+            dir: 'locale',
+            languages: supportedLanguages
+        }, function (err) {
+            console.log('foo');
+        });
+    });
 };
