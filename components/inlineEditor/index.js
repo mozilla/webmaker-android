873bf325a1208435f808aef1fce1ebf566c7cc9c
Aki Rose Braun <hi@akibraun.com>
2015-03-02 16:41:46 -0800

    Move up/down buttons never have a disabled state  Fixes #1375

--- components/inlineEditor/index.js
+++ components/inlineEditor/index.js
@@ -27,6 +27,15 @@ module.exports = {
     },
     methods: {
         startEditing: function () {
+
+            if (this.index === 0) {
+                this.$el.getElementsByClassName('inline-up')[0].classList.add('disabled');
+            }
+
+            if (this.index === this.app.blocks.length - 1) {
+                this.$el.getElementsByClassName('inline-down')[0].classList.add('disabled');
+            }
+
             this.isEditMode = true;
             this.$dispatch('inlineEditorStarted', {index: this.$index});
             this.$el.classList.add('active');
