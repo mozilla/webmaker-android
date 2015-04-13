d53924ed34d66068133b59fd276b269b50630313
Pomax <pomax@nihongoresources.com>
2015-04-13 12:32:48 -0700

    unified webmaker activity

--- app/src/main/java/mozilla/org/webmaker/Map.java
+++ app/src/main/java/mozilla/org/webmaker/Map.java
@@ -1,56 +1,8 @@
 package mozilla.org.webmaker;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.RelativeLayout;
-
-
-public class Map extends Activity {
-
-    private WmWebView mWebView = null;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.map_layout);
-
-        mWebView = new WmWebView(this, "map");
-        RelativeLayout layout = (RelativeLayout)findViewById(R.id.map_layout);
-        layout.addView(mWebView);
-    }
-
-    @Override
-    public void onDestroy() {
-        Log.v("wm", "onDestroy");
-        if (mWebView != null) {
-            mWebView.destroy();
-            mWebView = null;
-        }
-        super.onDestroy();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_map, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+public class Map extends WebmakerActivity {
+    public Map() {
+        super("map", R.id.map_layout, R.layout.map_layout, R.menu.menu_map);
     }
 }
+
