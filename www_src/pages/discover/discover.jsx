7cc277acbc4fd8b5be7d47547fc36e47e38907e6
Andrew Sliwinski <andrewsliwinski@acm.org>
2015-04-29 15:05:32 -0700

    WIP

--- www_src/pages/discover/discover.jsx
+++ www_src/pages/discover/discover.jsx
@@ -1,38 +1,61 @@
-var React = require('react');
-var render = require('../../lib/render.jsx');
-var Binding = require('../../lib/binding.jsx');
-var Link = require('../../components/link/link.jsx');
+var React = require('react/addons');
 var api = require('../../lib/api.js');
+var render = require('../../lib/render.jsx');
+var Link = require('../../components/link/link.jsx');
 
 var Discover = React.createClass({
-  mixins: [Binding],
+  mixins: [],
   getInitialState: function () {
+    this.list = [];
+
     return {
-      test: false
+      list: this.list,
+      offset: 0,
+      limit: 10,
+      locale: {
+        language: 'en',
+        country: 'us'
+      }
     };
   },
   componentWillMount: function () {
     api({
       uri: '/c0645e6953e9949f8e5c/raw/'
     }, function (err, body) {
-      console.dir(err);
-      console.dir(body);
-    });
+      // @todo Handle error state
+      if (!this.isMounted()) return;
+
+      // Update list
+      this.list = this.list.concat(body);
+      this.setState({
+        list: this.list
+      });
+    }.bind(this));
   },
   onClick: function () {
-    this.setState({test: !this.state.test});
+    // this.setState({test: !this.state.test});
   },
   render: function () {
-    return (
-      <div id="discover" className="demo">
-        <Link url="/map/123" href="/pages/map" className="tile tile-demo">
-          <div className="inner">
-            <div className="img">
-              <img src="../../img/toucan.svg" />
-            </div>
+    var cards = [];
+
+    console.dir(this.state.list);
+    for (var i = 0; i < this.state.list.length; i++) {
+      cards.push(
+        <Link url="/map/123" href="/pages/map" class="card">
+          <div class="thumbnail">
+            <img src="" />
+          </div>
+
+          <div class="meta">
+            {this.state.list[i].foo}
           </div>
         </Link>
-        <p><button onClick={this.onClick}>{this.state.test ? 'on' : 'off'}</button></p>
+      );
+    }
+
+    return (
+      <div id="discover">
+        {cards}
       </div>
     );
   }
