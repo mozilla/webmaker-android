01dac897235083b8a989754fff16f263dc7e7625
Gavin Lazar Suntop <gavin@gsuntop.com>
2015-05-01 12:07:23 -0700

    starting a built style guide

---
+++ www_src/pages/style-guide/style-guide.jsx
@@ -0,0 +1,62 @@
+var React = require('react');
+var render = require('../../lib/render.jsx');
+
+// Components
+var ColorGroup = require('../../components/color-group/color-group.jsx');
+var ColorSpectrum = require('../../components/color-spectrum/color-spectrum.jsx');
+var Range = require('../../components/range/range.jsx');
+var Tabs = require('../../components/tabs/tabs.jsx');
+
+var tabs = [
+  {
+    menu: <div>Tab 1</div>,
+    body: <div>Tab 1 body</div>
+  },
+  {
+    menu: <div>Tab 2</div>,
+    body: <div>Tab 2 body</div>
+  }
+];
+
+var StyleGuide = React.createClass({
+  render: function () {
+    return (
+      <div id="style-guide">
+        <h1>Style Guide</h1>
+
+        <h2>Typography</h2>
+
+        <h1>Heading H1</h1>
+        <h2>Heading H2</h2>
+        <h3>Heading H3</h3>
+        <h4>Heading H4</h4>
+        <h5>Heading H5</h5>
+        <h6>Heading H6</h6>
+
+        <p>Paragraph: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris.</p>
+
+        <h2>Colors</h2>
+
+        <h2>Components</h2>
+
+        <h3>Button (.btn)</h3>
+        <button className="btn">Button</button>
+
+        <h3>ColorGroup (JSX)</h3>
+        <ColorGroup/>
+
+        <h3>ColorSpectrum (JSX)</h3>
+        <ColorSpectrum value={'#fff'} onChange="" />
+
+        <h3>Range (JSX)</h3>
+        <Range min={0} max={255} unit="" />
+
+        <h3>Tabs (JSX)</h3>
+        <Tabs tabs={tabs} className="editor-options"></Tabs>
+
+      </div>
+    );
+  }
+});
+
+render(StyleGuide);
