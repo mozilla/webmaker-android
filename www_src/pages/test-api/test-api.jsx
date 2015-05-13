c1557b1b6adc1ad0ba4b75ca24cc2a702bd797cd
Kate Hudson <k88hudson@gmail.com>
2015-05-13 11:15:05 -0400

    api test page

---
+++ www_src/pages/test-api/test-api.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var render = require('../../lib/render.jsx');
+
+var ApiTest = React.createClass({
+  render: function () {
+    return (<div>
+      API tests
+    </div>);
+  }
+});
+
+render(ApiTest);
