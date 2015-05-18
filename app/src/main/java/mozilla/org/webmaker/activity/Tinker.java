f795075035eab9d0e2ee36330ce75fbeb5db05fc
RyanW <admin@ryan-w.me>
2015-05-18 18:29:03 -0400

    Fix possible compatibility issue on older SDK versions

--- app/src/main/java/mozilla/org/webmaker/activity/Tinker.java
+++ app/src/main/java/mozilla/org/webmaker/activity/Tinker.java
@@ -3,6 +3,7 @@ package mozilla.org.webmaker.activity;
 import android.app.ActionBar;
 import android.content.res.Resources;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -19,17 +20,19 @@ public class Tinker extends WebmakerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Custom styles
-        Resources res = getResources();
-        ColorDrawable plum = new ColorDrawable(getResources().getColor(R.color.plum));
-        int shadowPlum = res.getColor(R.color.shadow_plum);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Custom styles
+            Resources res = getResources();
+            ColorDrawable plum = new ColorDrawable(getResources().getColor(R.color.plum));
+            int shadowPlum = res.getColor(R.color.shadow_plum);
 
-        ActionBar actionBar = getActionBar();
-        actionBar.setBackgroundDrawable(plum);
+            ActionBar actionBar = getActionBar();
+            if (actionBar != null) actionBar.setBackgroundDrawable(plum);
 
-        Window window = getWindow();
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.setStatusBarColor(shadowPlum);
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(shadowPlum);
+        }
     }
 }
