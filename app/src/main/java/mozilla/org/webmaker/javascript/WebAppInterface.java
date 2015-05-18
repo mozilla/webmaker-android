a6fe6a8665a570f65aaace8094a4e60c648e9ac5
RyanW <admin@ryan-w.me>
2015-05-18 14:53:38 -0400

    Use the correct JavascriptInterface annotation

--- app/src/main/java/mozilla/org/webmaker/javascript/WebAppInterface.java
+++ app/src/main/java/mozilla/org/webmaker/javascript/WebAppInterface.java
@@ -3,9 +3,9 @@ package mozilla.org.webmaker.javascript;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
-import android.webkit.JavascriptInterface;
 
 import org.json.JSONObject;
+import org.xwalk.core.JavascriptInterface;
 
 import mozilla.org.webmaker.router.Router;
 
