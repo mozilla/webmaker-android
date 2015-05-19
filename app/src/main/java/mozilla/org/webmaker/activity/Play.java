2d1c79dfb2e63ab1ee1036c57402d8d529d457c4
Andrew Sliwinski <andrewsliwinski@acm.org>
2015-05-19 15:02:33 -0700

    Hook up 'play' activity. Re GH-1640

---
+++ app/src/main/java/mozilla/org/webmaker/activity/Play.java
@@ -0,0 +1,10 @@
+package mozilla.org.webmaker.activity;
+
+import mozilla.org.webmaker.R;
+import mozilla.org.webmaker.WebmakerActivity;
+
+public class Play extends WebmakerActivity {
+    public Play() {
+        super("project", R.id.page_layout, R.layout.page_layout, R.menu.menu_page);
+    }
+}
