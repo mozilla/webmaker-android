7ce69b78ddd5d4e716aafdf28ee07f4ecfd30425
Andrew Sliwinski <andrewsliwinski@acm.org>
2015-05-28 08:47:54 -0700

    Hook up sharing intent. Resolves GH-1713

---
+++ app/src/main/java/org/mozilla/webmaker/util/Share.java
@@ -0,0 +1,26 @@
+package org.mozilla.webmaker.util;
+
+import android.app.Activity;
+import android.content.Intent;
+import org.mozilla.webmaker.R;
+
+public class Share {
+
+    /**
+     * Launches a share intent using a specified subject and body.
+     *
+     * @param url URL to be appended to the share body
+     * @param activity Base activity
+     */
+    public static void launchShareIntent(final String url, final Activity activity) {
+        final Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+        final String shareSubject = activity.getString(R.string.share_subject);
+        final String shareBody = activity.getString(R.string.share_body).concat(" " + url);
+
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+
+        activity.startActivity(Intent.createChooser(shareIntent, "Share"));
+    }
+}
