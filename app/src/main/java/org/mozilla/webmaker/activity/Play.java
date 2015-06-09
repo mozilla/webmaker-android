2d6829aed86e1777d5a82fe584c3018c3fed48e5
Andrew Sliwinski <andrewsliwinski@acm.org>
2015-06-08 17:38:06 -0700

    Enable deep links for project player.

--- app/src/main/java/org/mozilla/webmaker/activity/Play.java
+++ app/src/main/java/org/mozilla/webmaker/activity/Play.java
@@ -1,5 +1,7 @@
 package org.mozilla.webmaker.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import org.json.JSONException;
 import org.mozilla.webmaker.R;
@@ -14,8 +16,25 @@ public class Play extends WebmakerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Set "mode" in route params
         try {
             routeParams.put("mode", "play");
+        } catch (JSONException e) {
+            // do nothing
+        }
+
+        // Get data from deep link (if available)
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+
+        // Set route params if valid deep link
+        if (data == null) return;
+        final String user = data.getQueryParameter("user");
+        final String project = data.getQueryParameter("project");
+        if (user == null || project == null) return;
+        try {
+            routeParams.put("user", user);
+            routeParams.put("project", project);
         } catch (JSONException e) {
             // do nothing
         }
