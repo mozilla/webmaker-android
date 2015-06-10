f065b1e441b5e3a2e7b7e9664a03278505c7306a
Andrew Sliwinski <andrewsliwinski@acm.org>
2015-06-09 20:38:24 -0700

    Resolve lint error

--- www_src/lib/keyboard.js
+++ www_src/lib/keyboard.js
@@ -6,7 +6,9 @@ module.exports = {
 
     for (var i = 0; i < elements.length; i++) {
       var t = parseInt(elements[i].getAttribute('tabIndex'), 10);
-      if (t === idx) elements[i].focus();
+      if (t === idx) {
+        elements[i].focus();
+      }
     }
   }
 };
