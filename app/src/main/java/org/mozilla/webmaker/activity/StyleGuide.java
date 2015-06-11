5ef9eb4d075226fb291fc4f5e529ee8b8a5567ff
RyanW <admin@ryan-w.me>
2015-06-10 21:42:41 -0400

    Add StyleGuide activity

---
+++ app/src/main/java/org/mozilla/webmaker/activity/StyleGuide.java
@@ -0,0 +1,10 @@
+package org.mozilla.webmaker.activity;
+
+import org.mozilla.webmaker.R;
+import org.mozilla.webmaker.WebmakerActivity;
+
+public class StyleGuide extends WebmakerActivity {
+    public StyleGuide() {
+        super("style-guide", R.id.styleguide_layout, R.layout.style_guide_layout, R.menu.menu_style_guide);
+    }
+}
