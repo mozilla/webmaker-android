17a6eb5bb5b7f4e9163360b90ab2199ab129f45a
Kate Hudson <k88hudson@gmail.com>
2015-06-12 20:24:57 -0400

    [#2211] Start on the right page

--- www_src/pages/project/project.jsx
+++ www_src/pages/project/project.jsx
@@ -1,6 +1,7 @@
 var React = require('react/addons');
 var update = React.addons.update;
 var assign = require('react/lib/Object.assign');
+var {parseJSON} = require('../../lib/jsonUtils');
 
 var render = require('../../lib/render.jsx');
 var router = require('../../lib/router');
@@ -29,10 +30,7 @@ var Project = React.createClass({
       loading: true,
       selectedEl: '',
       pages: [],
-      camera: {
-        x: 0,
-        y: 0
-      },
+      camera: {},
       zoom: DEFAULT_ZOOM,
       isPageZoomed: false
     };
@@ -75,8 +73,8 @@ var Project = React.createClass({
 
     if (window.Android) {
       var state = window.Android.getMemStorage('state');
-      if (typeof state !== 'undefined' && state !== '') {
-        state = JSON.parse(state);
+      if (this.state.params.mode === 'edit') {
+        state = parseJSON(state);
         if (state.params && state.params.project === this.state.params.project) {
           this.setState({
             selectedEl: state.selectedEl,
@@ -387,6 +385,18 @@ var Project = React.createClass({
     });
   },
 
+  findLandingPage: function(pages) {
+    var result;
+    // ... first, try to select 0, 0
+    pages.forEach((page) => {
+      if (page.coords.x === 0 && page.coords.y === 0) {
+        result = page;
+      }
+    });
+    // ... and if it was deleted, select the first page in the array
+    return result || pages[0];
+  },
+
   load: function () {
     this.setState({loading: true});
     api({uri: this.uri()}, (err, data) => {
@@ -406,18 +416,14 @@ var Project = React.createClass({
 
         state.pages = pages;
 
-        // If no currently selected page...
-        if (!this.state.selectedEl) {
-          // ... first, try to select 0, 0
-          pages.forEach((page) => {
-            if (page.coords.x === 0 && page.coords.y === 0) {
-              state.selectedEl = page.id;
-            }
-          });
-          // ... and if it was deleted, select the first page in the array
-          if (!state.selectedEl) {
-            state.selectedEl = pages[0].id;
-          }
+        var landingPage = this.findLandingPage(pages);
+        var focusTransform = this.cartesian.getFocusTransform(landingPage.coords, this.state.zoom);
+
+        if (this.state.params.mode === 'edit' && !this.state.selectedEl) {
+          state.selectedEl = landingPage.id;
+          state.camera = focusTransform;
+        } else if (typeof this.state.camera.x === 'undefined') {
+          state.camera = focusTransform;
         }
 
         this.setState(state);
@@ -562,7 +568,7 @@ var Project = React.createClass({
     };
 
     var boundingStyle = assign({
-        transform: `translate(${this.state.camera.x}px, ${this.state.camera.y}px) scale(${this.state.zoom})`,
+        transform: `translate(${this.state.camera.x || 0}px, ${this.state.camera.y || 0}px) scale(${this.state.zoom})`,
         opacity: this.state.pages.length ? 1 : 0
       },
       this.cartesian.getBoundingSize()
