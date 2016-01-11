b0201bf29503a6ff0cd9f978daaad4a20b6d1359
ryanw-se <email@ryan-w.me>
2016-01-11 17:45:12 -0500

    Don't display the ActionBar title within the TagList activity

--- app/src/main/java/org/mozilla/webmaker/activity/TagList.java
+++ app/src/main/java/org/mozilla/webmaker/activity/TagList.java
@@ -1,12 +1,23 @@
 package org.mozilla.webmaker.activity;
 
+import android.os.Bundle;
+
 import org.mozilla.webmaker.R;
 import org.mozilla.webmaker.WebmakerActivity;
 import org.mozilla.webmaker.router.SimpleRouter;
 
 public class TagList extends WebmakerActivity {
+
     public TagList() {
         super("tag-list", R.id.tag_list_layout, R.layout.tag_list_layout, R.menu.menu_tag_list);
         // SimpleRouter.getSharedRouter().call("/users/" + userId + "/projects/" + id + "/play");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (this.getActionBar() != null) {
+            this.getActionBar().setDisplayShowTitleEnabled(false);
+        }
     }
 }
